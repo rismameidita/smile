f33c27912d86ee77c5b4edc0e7123d76e291804f
rismameidita <rismameidi@yahoo.com>
2015-10-02 17:25:39 +0700

    Update cherry.hs

--- cherry.hs
+++ cherry.hs
@@ -15,3 +15,14 @@ length' [] = 0
 length' (x:xs) = 1 + (length' xs)
 
 --pembatas
+
+null' [] = True
+null' n = False
+
+--pembatas
+
+take' _[] = []
+take' 0 (x:xs) = []
+take' n (x:xs) = x : take' (n-1) xs
+
+head' (x:xs) = x
